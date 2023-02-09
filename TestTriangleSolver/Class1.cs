972b2c366b5c3993106035a75b0cb96a59cbb3f7
unknown <bdesai5071@conestogac.on.ca>
2023-02-09 17:15:21 -0500

    equilateral

---
+++ TestTriangleSolver/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+using NUnit.Framework;
+
+namespace TestTriangleSolver
+{
+    [TestFixture]
+    public class Equilateral
+    {
+        [Test]
+
+        public void AnalyzeTriangle_Input10and10and10_ValidEquilateral()
+        {
+            //Arrange
+            int firstSide = 10;
+            int secondSide = 10;
+            int thirdSide = 10;
+
+            string expected = "Based on all sides being equal, the type of triangle is an EQUILATERAL";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+    
+}
