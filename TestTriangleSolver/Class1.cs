5754e982d9ac535f2d3475827519918a0a393469
unknown <bdesai5071@conestogac.on.ca>
2023-02-09 17:21:41 -0500

    Isosceles

--- TestTriangleSolver/Class1.cs
+++ TestTriangleSolver/Class1.cs
@@ -29,5 +29,58 @@ namespace TestTriangleSolver
             Assert.AreEqual(expected, actual);
         }
     }
-    
+
+    public class Isosceles
+    {
+        [Test]
+        public void AnalyzeTriangle_Input10and10and15_ValidEquilateral()
+        {
+            //Arrange
+            int firstSide = 10;
+            int secondSide = 10;
+            int thirdSide = 15;
+
+            string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void AnalyzeTriangle_Input15and10and15_ValidEquilateral()
+        {
+            //Arrange
+            int firstSide = 15;
+            int secondSide = 10;
+            int thirdSide = 15;
+
+            string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input10and20and20_ValidEquilateral()
+        {
+            //Arrange
+            int firstSide = 10;
+            int secondSide = 20;
+            int thirdSide = 20;
+
+            string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
 }
