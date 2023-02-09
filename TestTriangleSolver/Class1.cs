6811da4d82a3cc5988289322233e71e5f9f9575d
unknown <bdesai5071@conestogac.on.ca>
2023-02-09 17:24:20 -0500

    scalene

--- TestTriangleSolver/Class1.cs
+++ TestTriangleSolver/Class1.cs
@@ -83,4 +83,90 @@ namespace TestTriangleSolver
         }
     }
 
+    public class Scalene
+    {
+        [Test]
+        public void AnalyzeTriangle_Input10and12and15_ValidEquilateral()
+        {
+            //Arrange
+            int firstSide = 10;
+            int secondSide = 12;
+            int thirdSide = 15;
+
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input11and13and14_ValidEquilateral()
+        {
+            //Arrange
+            int firstSide = 11;
+            int secondSide = 13;
+            int thirdSide = 14;
+
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input20and21and23_ValidEquilateral()
+        {
+            //Arrange
+            int firstSide = 20;
+            int secondSide = 21;
+            int thirdSide = 23;
+
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void AnalyzeTriangle_Input30and23and24_ValidEquilateral()
+        {
+            //Arrange
+            int firstSide = 30;
+            int secondSide = 23;
+            int thirdSide = 24;
+
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void AnalyzeTriangle_Input28and25and23_ValidEquilateral()
+        {
+            //Arrange
+            int firstSide = 28;
+            int secondSide = 25;
+            int thirdSide = 23;
+
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
 }
